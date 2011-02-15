c9cd2dda776e4071604d4ecd11b4f604eb03b762
arevalo.jul <arevalo.jul@gmail.com>
2011-02-15 15:09:55 +0000

    Le projet a été modifié. Désormais chaque tache est se trouve dans son fichier C.

--- Dev/Header/app_cfg.h
+++ Dev/Header/app_cfg.h
@@ -61,14 +61,8 @@
 *                                            TASK STACK SIZES
 *********************************************************************************************************
 */
-
-#define  APP_TASK_START_STK_SIZE            175
-
-#define  APP_TASK_GREEN_LED_STK_SIZE        175
-#define  APP_TASK_ORANGE_LED_STK_SIZE       175
-#define  APP_TASK_RED_LED_STK_SIZE        	175
-
 #define  APP_TASK_UART_TRANSMIT_STK_SIZE    175
+#define APP_TASK_START_STK_SIZE				175
 
 #if  uC_PROBE_OS_PLUGIN == DEF_ENABLED
 
